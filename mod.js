07354b45aef806369bcb72c388dc742d1ff10546
SansmaedaSerket <johnrice1997@gmail.com>
2022-03-04 03:28:22 -0500

    Initial commit (setup)

---
+++ mod.js
@@ -0,0 +1,9 @@
+const { Presence } = require('discord.js')
+
+module.exports = {
+    title: "Discord Mod",
+    summary: "Adds Discord integration.",
+    author: "SerketCity",
+    version: 0.1
+}
+
